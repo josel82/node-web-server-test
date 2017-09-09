adbe4e0b4c5f6975de3bf3d7d9ab333399b66399
josel82 <jose.padilla.dev@gmail.com>
2017-09-09 18:50:47 +0100

    Project page and link

--- server.js
+++ server.js
@@ -63,6 +63,13 @@ app.get('/about', (req, res)=>{
     pageTitle: 'About Page'
   });
 });
+
+app.get('/projects', (req, res)=>{
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
+  });
+});
+
 app.get('/bad',(req, res)=>{
   res.send({
     errorMessage:'Unable to handle the request.'
